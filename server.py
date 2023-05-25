2756c5041e0f35e3a0ae92060a59d01f5d057234
katzper.michno <katzper.michno@gmail.com>
2023-05-26 00:46:22 +0200

    First version of actual collaboration service

--- server.py
+++ server.py
@@ -1,26 +1,64 @@
 from simple_websocket_server import WebSocketServer, WebSocket
+import json
+import random
 
 class SimpleChat(WebSocket):
     def handle(self):
-        print(f'[MESSAGE][{self.address}] {self.data}')
+        clientId = clients[self]
+        print(f'[MESSAGE][{clientId}] {self.data}')
         for client in clients:
             if client != self:
                 client.send_message(self.data)
 
     def connected(self):
-        print(f'[CONNECTED] {self.address}')
-        for client in clients:
-            client.send_message(f'[CONNECTED] {self.address}')
-        self.send_message(f'[HELLO] {len(clients)}')
-        clients.append(self)
+        clientId = str(random.randint(0, 10**5))
 
-    def handle_close(self):
-        clients.remove(self)
-        print(f'[CLOSED] {self.address}')
-        for client in clients:
-            client.send_message(f'[CLOSED] {self.address}')
+        print(f'[CONNECTED] {clientId}')
 
-clients = []
+        newClient_message = {}
+        newClient_message['clientId'] = clientId
+        newClient_message['type'] = 1
+        
+        for client in clients:
+            client.send_message(json.dumps(newClient_message, separators=(',', ':')))
+        
+        clientsConnectedIds.append(clientId)
+        cursorsPositions[clientId] = None
+        
+        hello_message = {}
+        hello_message['document'] = document
+        hello_message['clientId'] = clientId
+        hello_message['clientsConnectedIds'] = clientsConnectedIds
+        hello_message['cursorsPositions'] = cursorsPositions
+        hello_message['type'] = 0
+        self.send_message(json.dumps(hello_message, separators=(',', ':')))
+
+        clients[self] = clientId
+
+    def handle_close(self): 
+        clientId = clients[self]
+        del clients[self]
+
+        disconnected_message = {}
+        disconnected_message['clientId'] = clientId
+        disconnected_message['type'] = 2
+        
+        for client in clients:
+            client.send_message(json.dumps(disconnected_message, separators=(',', ':')))
+        
+        clientsConnectedIds.remove(clientId)
+        del cursorsPositions[clientId]
+
+        print(f'[CLOSED] {clientId}')
+
+document = []
+clientsConnectedIds = []
+clients = {}
+cursorsPositions = {}
 
 server = WebSocketServer('', 3400, SimpleChat)
+
+print('Listening on port 3400')
+
 server.serve_forever()
+
